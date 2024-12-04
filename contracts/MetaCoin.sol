96a9101719deab45709be665e1a40710c4bf910e
badao <0xbadao1@proton.me>
2024-12-04 20:17:30 +0800

    fixed: SYMBOL value

--- contracts/MetaCoin.sol
+++ contracts/MetaCoin.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MetaCoin is ERC20 {
     string public constant NAME = "badaoERC20Token";
-    string public constant SYMBOL = "BD1";
+    string public constant SYMBOL = "BD";
     uint256 public constant INITIAL_SUPPLY = 10000;
 
     constructor() ERC20(NAME, SYMBOL) {
