e1ad8b5a2bb90d6cd1361258875c444d8d43cc30
badao <0xbadao1@proton.me>
2024-12-09 16:27:30 +0800

    增加一个合约

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,5 +1,7 @@
 const MetaCoin = artifacts.require("MetaCoin");
+const InfoContract = artifacts.require("InfoContract");
 
 module.exports = function (deployer) {
   deployer.deploy(MetaCoin);
+  deployer.deploy(InfoContract);
 };
